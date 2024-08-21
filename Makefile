47f65e75c547c6f7c1bb886b30527fd4874561e7
vera bashnyak <vera.bashnyak.1995@gmail.com>
2024-08-21 22:46:57 +1000

    add option and arguments

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+install:
+	npm ci
+
 gendiff:
 	node bin/gendiff.js
+
+lint:
+	npx eslint .
+
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
 
 program.parse();
 
