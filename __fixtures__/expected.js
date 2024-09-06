161835c826bb53f03ecf223578057216ff112389
vera bashnyak <vera.bashnyak.1995@gmail.com>
2024-09-06 23:29:49 +1000

    add tests

---
+++ __fixtures__/expected.js
@@ -0,0 +1,11 @@
+const expectedOutput =
+ `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+export default expectedOutput;
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import expectedOutput from '../__fixtures__/expected.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,4 +9,10 @@ const __dirname = path.dirname(__filename);
 import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename));
+
+test('compare files', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+
+    expect(genDiff(filepath1, filepath2)).toEqual(expectedOutput);
+});
