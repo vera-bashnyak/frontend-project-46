1c6ef878d6b850aa06ff02e8017aca71095c7b6c
vera bashnyak <vera.bashnyak.1995@gmail.com>
2024-10-09 00:56:10 +1000

    refactor: genDiff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import parse from './parsers.js';
-import formatName from './formatters/index.js';
 import findDiff from './find-diff.js';
 
-const genDiff = (filepath1, filepath2, format)=> {
+const genDiff = (filepath1, filepath2, formatName)=> {
   const [obj1, obj2] = parse(filepath1, filepath2);
-  const formatter = formatName(format);
-  return formatter(findDiff(obj1, obj2));
+  return formatName(findDiff(obj1, obj2));
 };
 
 export default genDiff;
